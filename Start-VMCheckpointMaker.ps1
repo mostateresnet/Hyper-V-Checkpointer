4eb7875e0a3decb988b213c7e34bc945cb497c2d
Clyde <theclyde@gmail.com>
2015-08-10 16:04:23 -0500

    Update Start-VMCheckpointMaker.ps1
    Added a bit of a catch to address the year 1599/1600 bug for the creation date. If this bug affects you, you should use my Start-RollingCheckpoints.ps1 script instead.

--- Start-VMCheckpointMaker.ps1
+++ Start-VMCheckpointMaker.ps1
@@ -10,30 +10,31 @@ It should not delete any manually created checkpoints (unless you rename them "A
 .NOTES   
 Name: VMCheckpointMaker$Deleter.ps1
 Author: Clyde Miller / ResNet @ Missouri State
-Version: 1.0
+Version: 1.1
 DateCreated: 2015-07-21
-DateUpdated: 
+DateUpdated: 2015-08-10
 
 .LINK
 http://thatclyde.com
 http://resnet.missouristate.edu
 
 .EXAMPLE   
-.\Get-Set-ADAccountasLocalAdministrator.ps1
+.\Get-Start-CheckpointManagement.ps1
 
 Description:
 Will create a checkpoint for each virtual machine running on your hyper-V server. 
-If there are any checkpoints that the script created previously that are 29 days old or older, they will be deleted.
+If there are any checkpoints that the script created previously that are $DaysKept old or older, they will be deleted.
 
 .NOTES:
-If you want to alter how long the backups are kept, change the number in this section of line 30: "(Get-Date).AddDays(-XX)"
+This script only kind of addresses the 1599/1600 bug, where checkpoints will report that they were initially created centuries ago. If it's a checkpoint that purports to be that old, this script will ignore it.
 
 #>
 #define local variables
 $VMs = '*'
 $DaysKept = 29
 
-#Finds Selected VMs (All by default), creates checkpoints for all with the name "Auto Checkpoint" followed by the date.
-Get-VM $VMs | Checkpoint-VM -SnapshotName "Auto Checkpoint $((Get-Date).toshortdatestring())"
-#Finds all checkpoints from the selected VMs with the name that begins "Auto Checkpoint", and, if they're older than the date in the $DaysKept variable, it deletes them.
-Get-VMSnapshot -VMName $VMs | Where-Object {$_.Name -Match "Auto Checkpoint*" -and $_.CreationTime -lt (Get-Date).AddDays(-$DaysKept) } | Remove-VMSnapshot
+#Finds Selected VMs (All by default), creates checkpoints for those VMs with the name "Auto Checkpoint" followed by the date (in a sortable format).
+Get-VM $VMs | Checkpoint-VM -SnapshotName "Auto Checkpoint $((Get-Date -Format s))"
+#Finds all checkpoints from the selected VMs with the name that begins "Auto Checkpoint", and, if they're older than the date in the $DaysKept variable, it deletes them. There is an additional "greater than 100 years" clause to catch the 1600 year bug.
+Get-VMSnapshot -VMName $VMs | Where-Object {$_.Name -Match "Auto Checkpoint*" -and $_.CreationTime -lt (Get-Date).AddDays(-$DaysKept) -and $_.CreationTime -gt (Get-Date).AddYears(-100)} | Remove-VMSnapshot
+
